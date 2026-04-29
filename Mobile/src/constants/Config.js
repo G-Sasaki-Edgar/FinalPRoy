5919fcc09095dbc8b71218aa08efec9f72a14bc7
G-Sasaki-Edgar <mufrus17@gmail.com>
2026-04-29 03:37:06 -0400

    Update Config.js

--- Mobile/src/constants/Config.js
+++ Mobile/src/constants/Config.js
@@ -1,7 +1,7 @@
 import { Platform } from 'react-native';
 
 // ⚠️ Cambiá esta IP por la IP de la laptop donde corre el backend
-const BACKEND_IP = '13.222.142.14'; 
+const BACKEND_IP = '52.23.237.244'; 
 
 export const API_URL = Platform.OS === 'web' 
   ? 'http://localhost:8000/api'        // Para navegador web
