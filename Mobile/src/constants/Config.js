ffe2479869cf3dbde1951d4858e372975f466d54
Edgar Sasaki <tu-correo@ejemplo.com>
2026-04-29 04:43:50 -0400

    actualizar IP y remover react native

--- Mobile/src/constants/Config.js
+++ Mobile/src/constants/Config.js
@@ -1,7 +1,7 @@
 import { Platform } from 'react-native';
 
 // ⚠️ Cambiá esta IP por la IP de la laptop donde corre el backend
-const BACKEND_IP = '52.23.237.244'; 
+const BACKEND_IP = '13.222.142.14'; 
 
 export const API_URL = Platform.OS === 'web' 
   ? 'http://localhost:8000/api'        // Para navegador web
